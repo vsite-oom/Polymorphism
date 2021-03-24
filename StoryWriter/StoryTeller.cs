cb85ac259fca869179788a7d125a663c794b3ae5
Topesa <pesa710@gmail.com>
2021-03-24 19:59:13 +0100

    Add HTML output to StoryTeller

--- StoryWriter/StoryTeller.cs
+++ StoryWriter/StoryTeller.cs
@@ -8,16 +8,30 @@ namespace Vsite.Oom.Polymorhism
 {
     class StoryTeller
     {
-        public void AddTitle(string title)
+        public void AddTitle(string title, bool IsHTML = false)
         {
-            Console.WriteLine(title);
-            Console.WriteLine(new string('=', title.Length));
+            if (IsHTML)
+            {
+                Console.WriteLine($"<h1>{title}</h1>");
+            }
+            else
+            {
+                Console.WriteLine(title);
+                Console.WriteLine(new string('=', title.Length));
+            }
         }
 
-        public void AddParagraph(string text)
+        public void AddParagraph(string text, bool IsHTML = false)
         {
-            Console.WriteLine(text);
-            Console.WriteLine();
+            if (IsHTML)
+            {
+                Console.WriteLine($"<p>{text}</p>");
+            }
+            else
+            {
+                Console.WriteLine(text);
+                Console.WriteLine();
+            }
         }
     }
 }
