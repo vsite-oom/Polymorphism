7e086a0f2f9d18aef37071c88a27623c2f4f598f
dajergov <dadoj@DESKTOP-4PTLH1S>
2021-03-24 20:23:12 +0100

    Introduce IFormatter interface and apply it to Story class

---
+++ StoryWriter-polymorphic/IFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.Polymorhism
+{
+    public interface IFormatter
+    {
+        void WriteHeading(string text, int level);
+        void WriteParagraph(string text);
+        void WriteHyperlink(string caption, string link);
+    }
+}
