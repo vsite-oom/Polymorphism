2347de446818d1f340a014ff80338b24519f8361
Senad Elezović <senad.elezovic@luvis.hr>
2021-03-24 19:59:54 +0100

    Add html format to story teller

--- StoryWriter/StoryTeller.cs
+++ StoryWriter/StoryTeller.cs
@@ -8,14 +8,24 @@ namespace Vsite.Oom.Polymorhism
 {
     class StoryTeller
     {
-        public void AddTitle(string title)
+        public void AddTitle(string title,bool isHtml=false)
         {
+            if(isHtml)
+            {
+                Console.WriteLine($"<h1>{title}</h1>");
+                return;
+            }
             Console.WriteLine(title);
             Console.WriteLine(new string('=', title.Length));
         }
 
-        public void AddParagraph(string text)
+        public void AddParagraph(string text,bool isHtml=false)
         {
+            if (isHtml)
+            {
+                Console.WriteLine($"<p>{text}</p>");
+                return;
+            }
             Console.WriteLine(text);
             Console.WriteLine();
         }
