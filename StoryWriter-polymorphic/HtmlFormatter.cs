923ed3fdee504e51666e4616adec6013f4ba9a20
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-03-27 00:04:24 +0100

    Implement HtmlFormatter

--- StoryWriter-polymorphic/HtmlFormatter.cs
+++ StoryWriter-polymorphic/HtmlFormatter.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Web.UI;
 
 namespace Vsite.Oom.Polymorhism
 {
     public class HtmlFormatter : IFormatter
     {
+        
         public void WriteHeading(string text, int level)
         {
-            throw new NotImplementedException();
+            switch (level)
+            {
+                case 1:
+                    writer.WriteLine($"<h1>{text}</h1>");
+                    break;
+                case 2:
+                    writer.WriteLine($"<h2>{text}</h2>");
+                    break;
+            }
         }
 
         public void WriteParagraph(string text)
         {
-            throw new NotImplementedException();
+            writer.WriteLine($"<p>{text}</p>");
         }
 
         public void WriteHyperlink(string caption, string link)
         {
-            throw new NotImplementedException();
+            writer.WriteLine($"<a href=\"{link}\">{caption}</a>");
         }
+
+        public override string ToString()
+        {
+            return writer.ToString();
+        }
+
+        private TextWriter writer = new StringWriter();
     }
 }
